b9033d62af0851730f0b6ecc61bac465fcaf2f3d
Charles Glasgow <brevenjh@gmail.com>
2022-11-17 15:37:51 -0600

    added routes for deleting and updating user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -109,3 +109,46 @@ router.post('/logout', (req, res) => {
         res.status(404).end();
     }
 });
+
+router.put('/:id', (req, res) => {
+
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: require.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: require.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
